c8100c7320f1b3909525b4c188b6d68c8832795c
Petar Pavlovic <pero356@gmail.com>
2023-11-08 15:09:34 +0100

    [WIP] Faster RCNN Model update

--- lib/architecture/FasterRCNNModel.py
+++ lib/architecture/FasterRCNNModel.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from lib.layers import Conv, ProposalGeneratorLayer, ProposalSelectorLayer
+from lib.layers import Conv, ProposalGeneratorLayer, ProposalSelectorLayer, ProposalTargetLayer
 from lib.loader_coco.classes import classes
 from typing import List
 
@@ -10,15 +10,17 @@ class FasterRCNNModel(tf.keras.Model): # ToDo Max Pool still not implemented
             name              : str,
             feature_extractor : tf.keras.layers.Layer,
             anchors           : List[List[int]],
+            inference         : bool = False,
     ):
-        super(SSDModel, self).__init__(name=name)
+        super(FasterRCNNModel, self).__init__(name=name)
 
         self.feature_extractor = feature_extractor
         self.anchors = anchors
         self.cls_conv = Conv(len(anchors) * 2, 1, name='rpn_cls_conv')
         self.bbox_conv = Conv(len(anchors) * 4, 1, name='rpn_bbox_conv')
-        self.proposal_generator_layer = ProposalGeneratorLayer()
+        self.proposal_generator_layer = ProposalGeneratorLayer(correct_proposals=False, clip=True, format='yxyx')
         self.proposal_selector_layer = ProposalSelectorLayer()
+        self.softmax = tf.keras.layers.Softmax()
 
     def call(self, inputs: tf.Tensor, training: bool = False):
 
@@ -28,12 +30,20 @@ class FasterRCNNModel(tf.keras.Model): # ToDo Max Pool still not implemented
 
         cls_conv = self.cls_conv(x)
 
+        # cls_conv = self.softmax(cls_conv)
+
         proposals = self.proposal_generator_layer(bbox_conv, inputs.shape, self.anchors)
 
-        proposals = self.proposal_selector_layer(proposals, cls_conv)
+        # proposals = self.proposal_selector_layer(proposals, cls_conv)
+
+        # if training:
+            # ToDo - also return bbox_conv and cls_conv because those need to be trained
 
         # ToDO continue
 
-        from IPython import embed
-        print('Model')
-        embed()
+        # ToDo - ROI Pooling
+        # ToDo - Cls Prediction
+        # ToDo - Bbox Prediction
+
+        # ToDo - fix return - this is just for now
+        return proposals, bbox_conv, cls_conv
