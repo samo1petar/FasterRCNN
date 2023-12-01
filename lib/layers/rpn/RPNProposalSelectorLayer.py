564464280b249c1c61b7e1bae3e6fbc3408266fe
Petar Pavlovic <pero356@gmail.com>
2023-12-01 13:21:02 +0100

    [add] remove small rpn proposals & percentage output

--- lib/layers/rpn/RPNProposalSelectorLayer.py
+++ lib/layers/rpn/RPNProposalSelectorLayer.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+from typing import Tuple
+from lib.tools.bbox import convert_from_exact_to_percentage, convert_yxyx_to_yxhw
 
 
 class RPNProposalSelectorLayer(tf.keras.layers.Layer):
@@ -10,11 +12,19 @@ class RPNProposalSelectorLayer(tf.keras.layers.Layer):
         super(RPNProposalSelectorLayer, self).__init__(name=name)
         self.cls_threshold = cls_threshold
         self.softmax = tf.keras.layers.Softmax()
-        self.nms_threshold = 0.5
-        self.nms_max_outputs = 100
+        self.nms_threshold = 0.7 # ToDo extract
+        self.nms_max_outputs = 100 # ToDo extract
+        self.min_size_threshold = 3
 
 
-    def call(self, proposals: tf.Tensor, cls_score: tf.Tensor) -> tf.Tensor:
+    def call(
+            self,
+            proposals         : tf.Tensor,
+            cls_score         : tf.Tensor,
+            input_image_shape : Tuple[int],
+            format            : str = 'percentage',
+            remove_small_proposals : bool = True,
+    ) -> tf.Tensor:
 
         proposals_shape = tf.shape(proposals)
 
@@ -46,16 +56,34 @@ class RPNProposalSelectorLayer(tf.keras.layers.Layer):
         # ToDo https://stackoverflow.com/questions/35330117/how-can-i-run-a-loop-with-a-tensor-as-its-range-in-tensorflow
         # ToDo take a look at example three at https://www.tensorflow.org/versions/r2.3/api_docs/python/tf/while_loop
 
+        if remove_small_proposals:
+            selected_boxes_yxhw = convert_yxyx_to_yxhw(selected_boxes)
+
+            mask_h = selected_boxes_yxhw[:, 2] > self.min_size_threshold
+            mask_w = selected_boxes_yxhw[:, 3] > self.min_size_threshold
+
+            mask = tf.logical_and(mask_h, mask_w)
+
+            selected_boxes = tf.boolean_mask(selected_boxes, mask)
+            selected_cls = tf.boolean_mask(selected_cls, mask)
+
         selected_boxes = tf.reshape(selected_boxes, [1, -1, 4])
         selected_cls = tf.reshape(selected_cls, [1, -1])
+
+        if format == 'percentage':
+            selected_boxes = convert_from_exact_to_percentage(selected_boxes, input_image_shape)
+        else:
+            raise ValueError # ToDo add other options
 
         return selected_boxes, selected_cls
 
 
 if __name__ == '__main__':
 
+    # ToDo remove main part
+
     import numpy as np
-    from ProposalGeneratorLayer import ProposalGeneratorLayer
+    from RPNProposalGeneratorLayer import RPNProposalGeneratorLayer
 
     bbox_deltas_np = np.zeros((1, 3, 3, 8))
     bbox_deltas_np[0, 0, 0, 1] = 1
@@ -65,7 +93,7 @@ if __name__ == '__main__':
     input_shape = tf.constant([1, 60, 100, 3], dtype=tf.float32)
     anchors = [[100, 100], [20, 20]] #, [7, 7], [10, 10]]
 
-    generate_proposal_layer = ProposalGeneratorLayer()
+    generate_proposal_layer = RPNProposalGeneratorLayer()
     proposals = generate_proposal_layer(bbox_deltas, input_shape, anchors)
 
     cls_deltas_np = np.random.random((1, 3, 3, 4))
