e77bb38902e6b6621f6b7f353050bc94469a0250
Petar Pavlovic <pero356@gmail.com>
2023-12-01 13:18:14 +0100

    [add] activation to FullyConnected class

--- lib/layers/FullyConnected.py
+++ lib/layers/FullyConnected.py
@@ -1,13 +1,19 @@
 import tensorflow as tf
+from lib.layers import Activation
 
 
 class FullyConnected(tf.keras.layers.Layer):
-    def __init__(self, units : int, **kwargs):
+    def __init__(self, units : int, activation: str = 'relu', **kwargs):
         super(FullyConnected, self).__init__(**kwargs)
         self.units = units
+        self.activation_name = activation
 
     def build(self, shape):
-        self.dense = tf.keras.layers.Dense(units=self.units)
+        if self.activation_name == 'none':
+            activation_class = None
+        else:
+            activation_class = Activation(self.activation_name)
+        self.dense = tf.keras.layers.Dense(units=self.units, activation=activation_class)
 
     def call(self, inputs, training: bool = True):
         return self.dense(inputs, training=training)
